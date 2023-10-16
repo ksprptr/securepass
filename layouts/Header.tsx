1fab90f5dc7a33c2d5a46b6853511ff838cc74d7
Petr Kašpar <92054010+kasparrpetr@users.noreply.github.com>
2023-10-16 22:05:06 +0200

    fixed navbar bars icon

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   const [navbar, setNavbar] = useState<boolean>(false);
@@ -27,20 +25,30 @@ export default function Header() {
           </ul>
           <button
             onClick={() => setNavbar(!navbar)}
-            className="md:hidden flex text-zinc-50"
+            className="md:hidden flex flex-col text-zinc-50 space-y-1"
           >
-            <FontAwesomeIcon icon={faBars} />
+            <div className="h-[3px] bg-zinc-50 rounded-full w-6" />
+            <div className="h-[3px] bg-zinc-50 rounded-full w-6" />
+            <div className="h-[3px] bg-zinc-50 rounded-full w-6" />
           </button>
         </div>
         <div className={`${navbar ? "block" : "hidden"} text-center`}>
           <ul className="py-4 text-xl space-y-4">
             <li>
-              <Link href="/generate" className="text-zinc-50">
+              <Link
+                href="/generate"
+                onClick={() => setNavbar(false)}
+                className="text-zinc-50"
+              >
                 Generate
               </Link>
             </li>
             <li>
-              <Link href="/algorithm" className="text-zinc-50">
+              <Link
+                href="/algorithm"
+                onClick={() => setNavbar(false)}
+                className="text-zinc-50"
+              >
                 Algorithm
               </Link>
             </li>
