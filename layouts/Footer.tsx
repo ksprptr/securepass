7fdef8464a72270fec8e7049b40dc1df746c40e0
Petr Kašpar <92054010+kasparrpetr@users.noreply.github.com>
2024-01-16 21:16:04 +0100

    updated year in footer

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-zinc-100 text-zinc-800">
       <div className="max-w-screen-xl mx-auto px-4 flex md:flex-row flex-col md:justify-between justify-center md:text-xl text-lg py-6 text-center">
-        <h1>&copy; Securepass 2022 - {new Date().getFullYear()}</h1>
+        <h1>&copy; Securepass {new Date().getFullYear()}</h1>
         <h2>
           Created by{" "}
           <Link href="https://kasparpetr.com" target="_blank" className="text-blue-600 hover:underline select-none">
