9a41ac318b479c0f6a28a63479af7b366af66d6f
Petr Kašpar <92054010+kasparrpetr@users.noreply.github.com>
2024-01-20 12:37:53 +0100

    updated packages and added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,10 @@ import SEO from "../next-seo.config";
 import Header from "@/layouts/Header";
 import Footer from "@/layouts/Footer";
 import { useRouter } from "next/router";
+import { Analytics } from "@vercel/analytics/react";
 import { DefaultSeo } from "next-seo";
 import { SavedPasswordsProvider } from "@/context/SavedPasswords";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,11 +15,14 @@ export default function App({ Component, pageProps }: AppProps) {
   const disabledPaths = ["/404", "/500"];
 
   return (
-    <SavedPasswordsProvider>
-      <DefaultSeo {...SEO} />
-      {!disabledPaths.includes(router.pathname) && <Header />}
-      <Component {...pageProps} />
-      {!disabledPaths.includes(router.pathname) && <Footer />}
-    </SavedPasswordsProvider>
+    <>
+      <Analytics />
+      <SavedPasswordsProvider>
+        <DefaultSeo {...SEO} />
+        {!disabledPaths.includes(router.pathname) && <Header />}
+        <Component {...pageProps} />
+        {!disabledPaths.includes(router.pathname) && <Footer />}
+      </SavedPasswordsProvider>
+    </>
   );
 }
