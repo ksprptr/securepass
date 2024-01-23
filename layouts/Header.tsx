134ef47a03f727268aa573ce52446bec08dcdb36
Petr Kašpar <92054010+kasparrpetr@users.noreply.github.com>
2024-01-23 15:52:22 +0100

    fixed navbar animation

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -26,48 +26,52 @@ export default function Header() {
 
   return (
     <nav className={`bg-zinc-100 z-10 fixed w-full text-zinc-800 ${colorChange && "shadow-xl"}`}>
-      <div className="max-w-screen-xl mx-auto px-4 py-6 text-xl flex items-center justify-between z-30">
-        <Link href="/" className="font-medium italic text-2xl hover:scale-105 duration-500 select-none">
-          Securepass
-        </Link>
-        <ul className="md:flex hidden gap-x-8">
-          <li>
-            <Link href="/" className="nav-btn">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/generate" className="nav-btn">
-              Generate
-            </Link>
-          </li>
-          <li>
-            <Link href="/save" className="nav-btn">
-              Save
-            </Link>
-          </li>
-        </ul>
-        <div ref={navRef} className="md:hidden flex text-2xl">
-          <FontAwesomeIcon icon={navbar ? faXmark : faBars} onClick={() => setNavbar(!navbar)} />
+      <div ref={navRef}>
+        <div className="max-w-screen-xl mx-auto px-4 py-6 text-xl flex items-center justify-between z-30">
+          <Link href="/" className="font-medium italic text-2xl hover:scale-105 duration-500 select-none">
+            Securepass
+          </Link>
+          <ul className="md:flex hidden gap-x-8">
+            <li>
+              <Link href="/" className="nav-btn">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/generate" className="nav-btn">
+                Generate
+              </Link>
+            </li>
+            <li>
+              <Link href="/save" className="nav-btn">
+                Save
+              </Link>
+            </li>
+          </ul>
+          <div className="md:hidden flex text-2xl">
+            <FontAwesomeIcon icon={navbar ? faXmark : faBars} onClick={() => setNavbar(!navbar)} />
+          </div>
         </div>
+        <motion.div initial={{ height: 0 }} animate={{ height: navbar ? "auto" : 0 }} transition={{ duration: 0.5 }} className="flex-col gap-y-2 text-right text-xl bg-zinc-100 overflow-hidden px-4">
+          <ul>
+            <li>
+              <Link href="/" onClick={() => setNavbar(false)} className="nav-btn">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/generate" onClick={() => setNavbar(false)} className="nav-btn">
+                Generate
+              </Link>
+            </li>
+            <li className="mb-4">
+              <Link href="/save" onClick={() => setNavbar(false)} className="nav-btn">
+                Save
+              </Link>
+            </li>
+          </ul>
+        </motion.div>
       </div>
-      <motion.ul initial={{ height: 0 }} animate={{ height: navbar ? "auto" : 0 }} transition={{ duration: 0.5 }} className="flex-col gap-y-2 text-right text-xl bg-zinc-100 overflow-hidden px-4">
-        <li>
-          <Link href="/" onClick={() => setNavbar(false)} className="nav-btn">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/generate" onClick={() => setNavbar(false)} className="nav-btn">
-            Generate
-          </Link>
-        </li>
-        <li className="mb-4">
-          <Link href="/save" onClick={() => setNavbar(false)} className="nav-btn">
-            Save
-          </Link>
-        </li>
-      </motion.ul>
     </nav>
   );
 }
--- utils/hooks/useOnClickOutside.ts
+++ utils/hooks/useOnClickOutside.ts
@@ -2,19 +2,19 @@ import { RefObject, useEffect } from "react";
 
 export const useOnClickOutside = (ref: RefObject<HTMLElement>, handler: (event: MouseEvent | TouchEvent) => void) => {
   useEffect(() => {
-    const listener = (event: MouseEvent | TouchEvent) => {
-      if (!ref.current || ref.current.contains(event.target as Node)) {
-        return;
-      }
-      handler(event);
-    };
-
-    document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
-
-    return () => {
-      document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
-    };
+      const listener = (event: MouseEvent | TouchEvent) => {
+        if (!ref.current || ref.current.contains(event.target as Node)) {
+          return;
+        }
+        handler(event);
+      };
+  
+      document.addEventListener("mousedown", listener);
+      document.addEventListener("touchstart", listener);
+  
+      return () => {
+        document.removeEventListener("mousedown", listener);
+        document.removeEventListener("touchstart", listener);
+      };
   }, [ref, handler]);
 };
