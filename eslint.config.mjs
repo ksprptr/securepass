1c111f855882a1617df0c879852f8b9187875e57
Petr Kašpar <petrkaspar@Petrs-MacBook-Pro.local>
2025-10-12 12:46:57 +0200

    chore: project prepared

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,79 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ["**/next-env.d.ts"],
   },
+  ...compat.extends(
+    "next",
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ),
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      prettier,
+      "simple-import-sort": simpleImportSort,
+    },
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      semi: "error",
+      "react/no-unescaped-entities": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-shadow": ["error"],
+      "@typescript-eslint/no-use-before-define": ["error"],
+      "no-use-before-define": "off",
+      "no-await-in-loop": "warn",
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "prefer-promise-reject-errors": "warn",
+      "spaced-comment": "error",
+      "no-duplicate-imports": "error",
+      "no-explicit-any": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_"
+        }
+      ],
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [["^\\u0000", "^@?w"], ["^@/"], ["^."], ["^.+.(css|scss)$"]]
+        }
+      ],
+      "simple-import-sort/exports": "error",
+      "import/no-named-as-default-member": "warn"
+    }
+  }
 ];
 
-export default eslintConfig;
+export default config;
